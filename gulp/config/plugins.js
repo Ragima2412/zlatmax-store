cf365f7dad57a6f6fa09de81326149734cecae77
Ragima2412 <ragimab@wizardsdev.com>
2022-07-26 11:18:14 +0400

    test

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -5,7 +5,7 @@ import browsersync from "browser-sync"; //Локальный сервер
 import newer from "gulp-newer"; // Проверка обновления
 import ifPlugin from "gulp-if"; // Условное ветвление
 
-
+  
 export const plugins = {
     replace: replace,
     plumber: plumber,
